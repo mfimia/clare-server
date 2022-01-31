1fc157956ba98abce0593f61cce11f1af39704cc
Martin Fimia <martin.fimia.osuna@hotmail.com>
2022-01-31 19:38:50 +0100

    added utility function to generate random referral codes

---
+++ utils/genReferral.js
@@ -0,0 +1,13 @@
+// returns a random string of 6 alphanumeric values
+const genReferral = () => {
+  let result = "";
+  const characters =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+  for (let i = 0; i < 6; i++) {
+    result += characters.charAt(Math.floor(Math.random() * characters.length));
+  }
+  return result;
+};
+
+module.exports = genReferral;
