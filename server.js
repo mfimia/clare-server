a976c83136c71eb4adc3a1a7405f7c60fd0c8ca8
Martin Fimia <martin.fimia.osuna@hotmail.com>
2022-02-02 13:55:03 +0100

    added auth route

--- server.js
+++ server.js
@@ -19,6 +19,7 @@ app.use(express.json({ extended: false }));
 
 // users route
 app.use("/api/users", require("./routes/users"));
+app.use("/api/auth", require("./routes/auth"));
 
 const PORT = process.env.PORT || 5000;
 
