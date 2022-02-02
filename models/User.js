574b4a388bd59ec7919dc347ba65da734e4427ce
Martin Fimia <martin.fimia.osuna@hotmail.com>
2022-02-02 13:54:38 +0100

    updated regex error message

--- models/User.js
+++ models/User.js
@@ -14,6 +14,7 @@ const UserSchema = new mongoose.Schema({
     required: true,
     match: [
       /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+      "Please enter a valid email format",
     ],
   },
   created_at: {
